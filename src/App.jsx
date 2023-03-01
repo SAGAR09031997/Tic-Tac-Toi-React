9df218c0efe3b124d0d23d8ec59ade59083f2acc
Sora History <sagarmehra0903@gmail.com>
2023-03-01 12:38:40 +0530

    add some new activities

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 // import Square from './components/Square'
+import { useState } from 'react';
 import './style.scss'
 import Board from './components/Board'
 
 function App() {
   
+  const [counter, stateCounter] = useState(1);
+
+  console.log('Pluse 1');
+  
+
+  const onBtnClick = () => {
+    
+    stateCounter(currentCounter => {
+      return currentCounter + 1
+    });
+  }
 
   return ( 
     <div className='app'>
-      <Board/>
-    </div>
+    
+          <div>
+            <button onClick={onBtnClick}>Click for know</button>
+          </div>
+          <div>{counter}</div>
+      </div>
+    
     
   )
 }
